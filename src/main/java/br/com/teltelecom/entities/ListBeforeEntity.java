1dbb4798d27f85e169b957ea2397f7be1e1afcbd
thiano pereira lima <thianolima@teltelecom.com.br>
2018-10-05 18:20:42 -0300

    adicionado lombok na entity

--- src/main/java/br/com/teltelecom/entities/ListBeforeEntity.java
+++ src/main/java/br/com/teltelecom/entities/ListBeforeEntity.java
@@ -1,22 +1,9 @@
 package br.com.teltelecom.entities;
 
+import lombok.Data;
+
+@Data
 public class ListBeforeEntity {
 	private String id;
-	private String name;
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
+	private String name;	
 }
