45e0b32037688d6fe005d591f17e2606ad8a7006
thiano pereira lima <thianolima@hotmail.com>
2018-10-09 20:57:58 -0300

    acrescentado swagger ao projeto

--- src/main/java/br/com/teltelecom/controllers/RelatorioController.java
+++ src/main/java/br/com/teltelecom/controllers/RelatorioController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/trello/relatorios")
+@RequestMapping("relatorios")
 public class RelatorioController {
 	
 	@Autowired
